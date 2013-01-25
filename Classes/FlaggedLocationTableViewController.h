45a00c7534f19330aca7d0394077cae86141c2ae
Anhong Guo <guoanhong15@gmail.com>
2013-01-24 22:11:02 -0500

    Cleaned unused views
    Possible bugs: iPhone5 issue, unuploaded trips
    Next step: data structure and upload, add Flagged view

---
+++ Classes/FlaggedLocationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlaggedLocationTableViewController.h
+//  Cycle Atlanta
+//
+//  Created by Guo Anhong on 13-1-24.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlaggedLocationTableViewController : UITableViewController
+
+@end
